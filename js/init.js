acf77396ba446e8acd66d018c85d822fce39e923
affandi-arc <farisafn008@gmail.com>
2022-09-23 16:29:20 +0700

    update a code

--- js/init.js
+++ js/init.js
@@ -137,48 +137,70 @@
 /*	contact form
 ------------------------------------------------------*/
 
-   $('form#contactForm button.submit').click(function send() {
 
-      $('#image-loader').fadeIn();
+   // $('form#contactForm button.submit').click(function send() {
 
-      var contactName = $('#contactForm #contactName').val();
-      var contactEmail = $('#contactForm #contactEmail').val();
-      var contactSubject = $('#contactForm #contactSubject').val();
-      var contactMessage = $('#contactForm #contactMessage').val();
+   //    $('#image-loader').fadeIn();
 
-      var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
-               '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
+   //    var contactName = $('#contactForm #contactName').val();
+   //    var contactEmail = $('#contactForm #contactEmail').val();
+   //    var contactSubject = $('#contactForm #contactSubject').val();
+   //    var contactMessage = $('#contactForm #contactMessage').val();
 
-      $.ajax({
+   //    var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
+   //             '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
 
-	      type: "POST",
-	      url: "/inc/sendEmail.php",
-	      data: data,
-	      success: function(msg) {
+   //    $.ajax({
 
-            // Message was sent
-            if (msg == 'OK') {
-               $('#image-loader').fadeOut();
-               $('#message-warning').hide();
-               $('#contactForm').fadeOut();
-               $('#message-success').fadeIn();   
-            }
-            // There was an error
-            else {
-               $('#image-loader').fadeOut();
-               $('#message-warning').html(msg);
-	            $('#message-warning').fadeIn();
-            }
+	//       type: "POST",
+	//       url: "https://script.google.com/macros/s/AKfycbw1yELQYtvkrvKxIJWC20hQ51bUYS1Z96uPoGM0QpEL6pcKcplPXrvM6sorQntgSrP1MQ/exec",
+	//       data: data,
+	//       success: function(msg) {
 
-	      }
+   //          // Message was sent
+   //          if (msg == 'OK') {
+   //             $('#image-loader').fadeOut();
+   //             $('#contactForm').fadeOut();
+   //             $('#message-success').fadeIn();   
+   //          }
+   //          // There was an error
+   //          else {
+   //             $('#image-loader').fadeOut();
+   //             $('#message-warning').html(msg);
+	//             $('#message-warning').fadeIn();
+   //          }
 
-      });
-      return false;
-   });
+	//       }
 
-
+   //    });
+   //    return false;
+   // });
+   
+   
 });
 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwujSNjVC6l7ZKSDJcJ82_hJP2ghkS9VNcRKYIJBAXaVbM0som5e6-xL9qGk5LrfpFpmg/exec'
+const form = document.forms['contactForm']
+const imgLoading = document.querySelector('#image-loader')
+const statusSubmit = document.querySelector('#message-success')
+const failSubmit = document.querySelector('#message-warning')
+
+form.addEventListener('submit', e => {
+   e.preventDefault();
+   imgLoading.style.display = 'inline'; ;
+   fetch(scriptURL, { method: 'POST', body: new FormData(form)
+})
+   .then(response => {console.log('Success!', response)
+   imgLoading.style.display = 'none';
+   statusSubmit.style.display = 'block';
+   form.reset();
+})
+
+   .catch(error => {console.error('Error!', error.message)
+   imgLoading.style.display = 'none';
+   failSubmit.style.display = 'block';
+})
+})
 
 
 
